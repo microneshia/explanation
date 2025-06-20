a28410bee0a0f255e86e48e2122e7b9a0b8fb741
microneshia <shuma_t_2023@outlook.jp>
2025-06-20 20:41:58 +0900

    feat: Replace all project files with new version

--- js/article-list.js
+++ js/article-list.js
@@ -30,7 +30,6 @@ const explanationArticles = [
 
 // その他のコンテンツ (contents/ フォルダ内) のファイル名とタグ情報を管理
 const contents = [
-    { file: 'video-downloader.html',    tags: ['コンテンツ', 'ツール', '動画', 'ダウンローダー', 'yt-dlp'] },  // 17
     { file: 'gitguide.html',            tags: ['コンテンツ', 'Git', 'GitHub', 'VS Code', 'チュートリアル'] },  // 16
     { file: 'bad-ui-showcase.html',     tags: ['コンテンツ', 'ジョーク', 'UI/UX', 'Webデザイン', 'JavaScript'] },  // 15
     { file: 'music.html',               tags: ['コンテンツ', 'ツール', '音楽', 'メディアプレーヤー', 'Web Audio API'] },  // 14
